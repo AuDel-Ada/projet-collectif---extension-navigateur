c625053603f8c18aea213370c7207a15f34ba724
Fhommand <85489785+Fhommand@users.noreply.github.com>
2021-09-09 16:30:01 +0200

    map

--- Projet/app.js
+++ Projet/app.js
@@ -1,17 +1,30 @@
-function permuteElement() {
-  var elements = document.getElementsByTagName("*");
+const hardWordsToSoftWordsFR = new Map()
+
+hardWordsToSoftWordsFR.set('des', 'soleil')
+hardWordsToSoftWordsFR.set("la", "danse")
+
+
+
+
+function permuteElement () {
+  var elements = document.getElementsByTagName('*')
   for (var i = 0; i < elements.length; i++) {
-    var element = elements[i];
+    var element = elements[i]
     for (var j = 0; j < element.childNodes.length; j++) {
-      var node = element.childNodes[j];
+      var node = element.childNodes[j]
       if (node.nodeType === 3) {
-        var text = node.nodeValue;
-        var replacedText = text.replace(/des/gi, "soleil");
-        if (replacedText !== text) {
-          element.replaceChild(document.createTextNode(replacedText), node);
+        var text = node.nodeValue
+
+        for (let hardWord of hardWordsToSoftWordsFR.keys()) {
+          var softWord = hardWordsToSoftWordsFR.get(hardWord)
+          var replacedText = text.replace(hardWord, softWord)
+          if (replacedText !== text) {
+            element.replaceChild(document.createTextNode(replacedText), node)
+          }
         }
       }
     }
   }
 }
-setTimeout(permuteElement, 5000);
+
+permuteElement()
