706f3670af696807fd3c54b503935e036bf6c1e4
AuDel-Ada <85498271+AuDel-Ada@users.noreply.github.com>
2021-09-10 10:15:32 +0200

    Add files via upload
    Utilisation d'un dictionnaire pour intégrer les mots à remplacer

--- Projet/app.js
+++ Projet/app.js
@@ -3,22 +3,19 @@ const hardWordsToSoftWordsFR = new Map()
 hardWordsToSoftWordsFR.set('des', 'soleil')
 hardWordsToSoftWordsFR.set("la", "danse")
 
+function permuteElement() {
 
-
-
-function permuteElement () {
-  var elements = document.getElementsByTagName('*')
-  for (var i = 0; i < elements.length; i++) {
-    var element = elements[i]
-    for (var j = 0; j < element.childNodes.length; j++) {
-      var node = element.childNodes[j]
-      if (node.nodeType === 3) {
-        var text = node.nodeValue
-
-        for (var hardWord of hardWordsToSoftWordsFR.keys()) {
-          console.log(hardWord)
-          var softWord = hardWordsToSoftWordsFR.get(hardWord)
-          console.log(softWord)
+  for (var hardWord of hardWordsToSoftWordsFR.keys()) {
+    console.log(hardWord)
+    var softWord = hardWordsToSoftWordsFR.get(hardWord)
+    console.log(softWord)
+    var elements = document.getElementsByTagName('*')
+    for (var i = 0; i < elements.length; i++) {
+      var element = elements[i]
+      for (var j = 0; j < element.childNodes.length; j++) {
+        var node = element.childNodes[j]
+        if (node.nodeType === 3) {
+          var text = node.nodeValue
           var replacedText = text.replace(hardWord, softWord)
           if (replacedText !== text) {
             element.replaceChild(document.createTextNode(replacedText), node)
