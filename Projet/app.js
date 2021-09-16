a677c83a7aba7668c557e98cb77d71cfa53f18c8
LaurieCLu <59483905+LaurieClu@users.noreply.github.com>
2021-09-16 15:24:00 +0200

    modification de la couleur des éléments de texte remplacés

--- Projet/app.js
+++ Projet/app.js
@@ -16,59 +16,17 @@ function permuteElement() {
       for (var j = 0; j < element.childNodes.length; j++) {
         var node = element.childNodes[j]
         if (node.nodeType === 3) {
-          var text = node.nodeValue
-          var replacedText = text.replace(hardWord, softWord)
+          var text = node.nodeValue;
+          var replacedText = text.replace(hardWord,softWord);
           if (replacedText !== text) {
-            element.replaceChild(document.createTextNode(replacedText), node)
+            element.innerHTML =  text.replace(hardWord, "<span style='color:red'>" + softWord + "</span>");
           }
         }
       }
     }
   }
 }
-
-permuteElement()
-
-// const hardWordsToSoftWordsFR = new Map()
-
-// hardWordsToSoftWordsFR.set(' des ', ' soleil ')
-// hardWordsToSoftWordsFR.set(" la ", " danse ")
-// hardWordsToSoftWordsFR.set(" the ", " dance ")
-
-// function permuteElement() {
-
-//   for (var hardWord of hardWordsToSoftWordsFR.keys()) {
-//     console.log(hardWord)
-//     var softWord = hardWordsToSoftWordsFR.get(hardWord)
-//     console.log(softWord)
-//     var elements = document.getElementsByTagName('*')
-//     for (var i = 0; i < elements.length; i++) {
-//       var element = elements[i]
-//       for (var j = 0; j < element.childNodes.length; j++) {
-//         var node = element.childNodes[j]
-//         if (node.nodeType === 3) {
-//           var text = node.nodeValue;
-//           var replacedText = text.replace(hardWord,softWord);
-//           if (replacedText !== text) {
-//             var newSpan = document.createElement('span');
-//             coloredTextNode = element.replaceChild(document.createTextNode(replacedText), node);
-//             var parent = coloredTextNode.parentElement
-//             parent.appendChild(newSpan);
-       
-//             newSpan.style.color = "red";
-
-            
-//           }
-//         }
-//       }
-//     }
-//   }
-// }
-
-// setInterval(permuteElement, 3000)
-
-// // var text = result.replace(_word, "<span style='color:red'>" + _word "</span>");
-// // var _test = document.createTextNode("text");
+setInterval(permuteElement, 3000)
 
 // function createColor() {
 //   const colors = ["#1F85DE", "#DBDE1F"]
